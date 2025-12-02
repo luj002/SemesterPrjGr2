ffee68050c3b8922b5dd395c80ddca21ab86f04f
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2025-12-02 13:34:53 +0100

    BoatSpaceRepository

--- SemesterProjectGr2/SejlklubLibrary/Interfaces/IBoatSpaceRepository.cs
+++ SemesterProjectGr2/SejlklubLibrary/Interfaces/IBoatSpaceRepository.cs
@@ -3,6 +3,6 @@
     int Count { get; }
     List<BoatSpace> GetAll();
     void Add(BoatSpace boatSpace);
-    Boat? GetBoatSpaceByNumber(int number);
+    BoatSpace? GetBoatSpaceByNumber(int number);
     void Remove(int number);
 }
--- SemesterProjectGr2/SejlklubLibrary/Services/BoatSpaceRepository.cs
+++ SemesterProjectGr2/SejlklubLibrary/Services/BoatSpaceRepository.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SejlklubLibrary.Services
+﻿
+public class BoatSpaceRepository : IBoatSpaceRepository
 {
-    public class BoatSpaceRepository
-    {
+    #region Instance fields
+    private Dictionary<int, BoatSpace> _boatSpaces;
+    #endregion
 
+    #region Properties
+    public int Count
+    {
+        get { return _boatSpaces.Count; }
     }
+    #endregion
+
+    #region Constructor
+    public BoatSpaceRepository()
+    {
+        _boatSpaces = new Dictionary<int, BoatSpace>();
+    }
+    #endregion
+
+    #region Methods
+    public void Add(BoatSpace boatSpace)
+    {
+        if (!_boatSpaces.TryAdd(boatSpace.Number, boatSpace))
+            throw new RepositoryException(RepositoryExceptionType.Add, "BoatSpace with that number already exists");
+    }
+
+    public List<BoatSpace> GetAll()
+    {
+        return _boatSpaces.Values.ToList();
+    }
+
+    public BoatSpace? GetBoatSpaceByNumber(int number)
+    {
+        return (_boatSpaces.ContainsKey(number)) ? _boatSpaces[number] : null;
+    }
+
+    public void Remove(int number)
+    {
+        _boatSpaces.Remove(number);
+    }
+    #endregion
 }
+
