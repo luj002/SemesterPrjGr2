f864de0610e868fe33e5793c8a15693286f60647
Robin Bauer Nørby <ron001@edu.zealand.dk>
2025-12-02 12:43:32 +0100

    event repository tested. It works.

--- SemesterProjectGr2/ConsoleRobin/Program.cs
+++ SemesterProjectGr2/ConsoleRobin/Program.cs
@@ -1,3 +1,8 @@
 ﻿EventRepository eRep = new EventRepository();
+
+Adminstrator coolAdmin = new Adminstrator("Poul","dunno","aaa@gmail.com",DateTime.Now,ProjectEnums.MemberType.SENIOR);
+Event coolEvent = new Event("sus","suss",DateTime.Now,DateTime.Now,coolAdmin);
+eRep.AddEvent(coolEvent);
+
+eRep.RemoveEvent(coolEvent);
 Lua.print(eRep.GetAll());
-eRep.GetEventByID(4);
