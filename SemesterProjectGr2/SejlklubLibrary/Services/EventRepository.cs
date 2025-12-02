39303f240fc68620d2feefbc8926408142102c15
Robin Bauer Nørby <ron001@edu.zealand.dk>
2025-12-02 12:05:55 +0100

    event repository

--- SemesterProjectGr2/SejlklubLibrary/Services/EventRepository.cs
+++ SemesterProjectGr2/SejlklubLibrary/Services/EventRepository.cs
@@ -1,4 +1,51 @@
-﻿public class EventRepository
+﻿public class EventRepository : IEventRepository
 {
-    public 
+    private Dictionary<int,Event> _events;
+    public int Count { 
+        
+        get
+        {
+            return _events.Count;
+        }
+
+    }
+
+    public EventRepository()
+    {}
+
+    public List<Event> GetAll()
+    {
+        return _events.Select(pair => pair.Value).ToList();
+    }
+
+    public Event? GetEventByID(int id)
+    {
+        if (_events[id] != null)
+        {
+            return _events[id];
+        }
+
+        else
+        {
+            throw new Exception("invalid id given");
+        }
+    }
+
+    public void AddEvent(Event givenEvent)
+    {
+        if (_events[givenEvent.Id] == null)
+        {
+            _events[givenEvent.Id] = givenEvent;
+        }
+
+        else
+        {
+            throw new Exception("invalid event given");
+        }
+    }
+
+    public void RemoveEvent(Event givenEvent)
+    {
+        _events.Remove(givenEvent.Id);
+    }
 }
