be0d8ee42071ba84db04ce36c9a5f0fea93252dc
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2025-12-02 11:12:47 +0100

    Added interfaces

--- SemesterProjectGr2/SejlklubLibrary/Interfaces/IBlogEntryRepository.cs
+++ SemesterProjectGr2/SejlklubLibrary/Interfaces/IBlogEntryRepository.cs
@@ -1,3 +1,8 @@
 ﻿public interface IBlogEntryRepository
 {
+    int Count { get; }
+    List<BlogEntry> GetAll();
+    void Add(BlogEntry blogEntry);
+    Boat? GetBlogEntryById(int id);
+    void Remove(int id);
 }
