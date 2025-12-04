05127563c0bdd90bf5ccca64e779afac6088a3e1
Robin Bauer Nørby <ron001@edu.zealand.dk>
2025-12-04 09:37:57 +0100

    booking repository

--- SemesterProjectGr2/SejlklubLibrary/Services/BookingRepository.cs
+++ SemesterProjectGr2/SejlklubLibrary/Services/BookingRepository.cs
@@ -1,4 +1,52 @@
 ﻿public class BookingRepository
 {
+    private Dictionary<int,Booking> _bookings = new Dictionary<int,Booking>();
+    public int Count
+    {
 
+        get
+        {
+            return _bookings.Count;
+        }
+
+    }
+
+    public BookingRepository()
+    {}
+
+    public List<Booking> GetAll()
+    {
+        return _bookings.Values.ToList();
+    }
+
+    public void AddBooking(Booking givenBooking)
+    {
+        if (!_bookings.ContainsKey(givenBooking.Id))
+        {
+            _bookings[givenBooking.Id] = givenBooking;
+        }
+
+        else
+        {
+            throw new RepositoryException(RepositoryExceptionType.Add,"Given booking is already in the bookings dictionary.");
+        }
+    }
+
+    public void RemoveBooking(Booking givenBooking)
+    {
+        if (_bookings.ContainsValue(givenBooking))
+        {
+            _bookings.Remove(givenBooking.Id);
+        }
+    }
+
+    public Booking? GetBookingByID(int id)
+    {
+        if (_bookings.ContainsKey(id))
+        {
+            return _bookings[id];
+        }
+
+        return null;
+    }
 }
