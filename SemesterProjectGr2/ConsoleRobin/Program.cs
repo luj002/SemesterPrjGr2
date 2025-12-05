f2be98c63b26cd12b30ecfd5bc198ccfa8d24d24
MidnightSuns <omgzergrushguy@gmail.com>
2025-12-05 10:32:11 +0100

    acually done does the thing

--- SemesterProjectGr2/ConsoleRobin/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿EventRepository eRep = new EventRepository();
-
-Adminstrator coolAdmin = new Adminstrator("Poul","dunno","aaa@gmail.com",DateTime.Now,MemberType.SENIOR);
-Event coolEvent = new Event("sus","suss",DateTime.Now,DateTime.Now,coolAdmin);
-eRep.AddEvent(coolEvent);
-
-eRep.RemoveEvent(coolEvent);
-Lua.print(eRep.GetAll());
