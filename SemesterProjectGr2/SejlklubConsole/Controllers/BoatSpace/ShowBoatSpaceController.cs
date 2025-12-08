46ec4bd6db78eb0346586d3424260757560ae058
Nikolaj Bjerregaard Uttrup <nikbuzealand@gmail.com>
2025-12-08 12:09:13 +0100

    Add, Remove, and Show BoatSpaceControllers

---
+++ SemesterProjectGr2/SejlklubConsole/Controllers/BoatSpace/ShowBoatSpaceController.cs
@@ -0,0 +1,29 @@
+﻿public class ShowBoatSpaceController
+{
+    #region Instance Fields
+    private IBoatSpaceRepository _boatSpaceRepository;
+    #endregion
+
+    #region Constructors
+    public ShowBoatSpaceController(IBoatSpaceRepository boatSpaceRepository)
+    {
+        _boatSpaceRepository = boatSpaceRepository;
+    }
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+    public void ShowAllBoatSpaces()
+    {
+        foreach (BoatSpace boatSpace in _boatSpaceRepository.GetAll())
+        {
+            Console.WriteLine(boatSpace);
+        }
+    }
+    #endregion
+}
+
+
