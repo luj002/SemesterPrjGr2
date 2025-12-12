ddf7b6e2e43df49a228d3a8d72b2e0d0dbf0e108
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2025-12-12 13:39:00 +0100

    Booking activation and finalization

--- SemesterProjectGr2/SejlklubConsole/Controllers/Booking/BookingActiveController.cs
+++ SemesterProjectGr2/SejlklubConsole/Controllers/Booking/BookingActiveController.cs
@@ -4,6 +4,14 @@
     private IBookingRepository _bookingRepository;
     private Booking _booking;
     #endregion
+
+    #region Properties
+    public Booking Booking
+    {
+        get { return _booking; }
+    }
+    #endregion
+
     #region Constructor
     public BookingActiveController(IBookingRepository bookingRepository, Booking booking)
     {
@@ -15,16 +23,52 @@
     #region Methods
     public void ActivateBooking()
     {
-        bool activate = Helpers.YesOrNoKey($"Do you want to activate {_booking.Id}?");
+        if (_booking.IsActive)
+        {
+            Console.WriteLine($"Booking {_booking.Id} is already active.");
+            Console.ReadKey();
+            return;
+        }
+
+        if (_bookingRepository.GetAllArchived().Contains(_booking))
+        {
+            Console.WriteLine($"Booking {_booking.Id} is archived and cannot be activated.");
+            Console.ReadKey();
+            return;
+        }
+
+        bool activate = Helpers.YesOrNo($"Do you want to activate {_booking.Id}?");
         if (activate)
         {
             _booking.IsActive = true;
-            //_bookingRepository.Update(_booking);
             Console.WriteLine($"Booking {_booking.Id} is now active.");
         }
         else
         {
             Console.WriteLine($"Booking {_booking.Id} remains inactive.");
+        }
+        Console.ReadKey();
+    }
+
+    public void FinalizeBooking()
+    {
+        if (!_booking.IsActive)
+        {
+            Console.WriteLine($"Booking {_booking.Id} is not active and cannot be finalized.");
+            Console.ReadKey();
+            return;
+        }
+
+        bool finalize = Helpers.YesOrNo($"Do you want to finalize {_booking.Id}?");
+        if (finalize)
+        {
+            _booking.IsActive = false;
+            _bookingRepository.Archive(_booking.Id);
+            Console.WriteLine($"Booking {_booking.Id} is now finalized.");
+        }
+        else
+        {
+            Console.WriteLine($"Booking {_booking.Id} remains unfinalized.");
         }
         Console.ReadKey();
     }
