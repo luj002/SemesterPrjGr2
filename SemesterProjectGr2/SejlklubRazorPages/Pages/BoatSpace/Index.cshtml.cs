cd2e2c62786d08eb762352d239522960bd3df7f4
Nikolaj Bjerregaard Uttrup <nikbuzealand@gmail.com>
2025-12-16 10:05:11 +0100

    First draft of BoatSpace RazorPage

---
+++ SemesterProjectGr2/SejlklubRazorPages/Pages/BoatSpace/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SejlklubRazorPages.Pages.BoatSpace
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
